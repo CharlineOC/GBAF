e1f107930139c069b16ae8a88aaa383ba83adb99
CharlineOC <charline.ging@gmail.com>
2021-01-26 12:50:21 +0100

    Update profil_post.php

--- profil_post.php
+++ profil_post.php
@@ -30,18 +30,27 @@ if (!empty($_POST['nom']) AND ($data_user['nom'] != $_POST['nom']))
 {
 	$req = $bdd->prepare('UPDATE account SET nom=:nom WHERE id_user = :id_user'); 
 	$req->execute(array('id_user'=>$_SESSION['id_user'], 'nom'=>$_POST['nom']));
+
+	
+	$_SESSION['nom'] = $_POST['nom'];
 }
 
 if (!empty($_POST['prenom']) AND ($data_user['prenom'] != $_POST['prenom']))
 {
 	$req = $bdd->prepare('UPDATE account SET prenom=:prenom WHERE id_user = :id_user'); 
 	$req->execute(array('id_user'=>$_SESSION['id_user'], 'prenom'=>$_POST['prenom']));
+
+
+	$_SESSION['prenom'] = $_POST['prenom'];
 }
 
 if (!empty($_POST['pseudo']) AND ($data_user['pseudo'] != $_POST['pseudo']))
 {
 	$req = $bdd->prepare('UPDATE account SET pseudo=:pseudo WHERE id_user = :id_user'); 
 	$req->execute(array('id_user'=>$_SESSION['id_user'], 'pseudo'=>$_POST['pseudo']));
+	
+
+	$_SESSION['pseudo'] = $_POST['pseudo'];
 }
 
 if (!empty($_POST['motdepasse']) AND (!$isSamePassword))
@@ -65,3 +74,5 @@ if (!empty($_POST['reponsesecrete']) AND ($data_user['reponsesecrete'] != $_POST
 header('Location: profil.php');
 
 ?>
+
+<!-- rajouter htmlspecialchars -->
